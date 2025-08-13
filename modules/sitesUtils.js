a754a419380017484569d406d3876994c453b521
kovaltar <kovaltar@gmail.com>
2025-08-13 18:52:38 +0200

    Renaiming sitesMonitor to sitesUtils and implementation getting sites array from sitesUtils

--- modules/sitesUtils.js
+++ modules/sitesUtils.js
@@ -27,6 +27,32 @@ async function ping(sites) {
   return results;
 }
 
-export const sitesMonitor = {
+async function getSites() {
+  let sitesArr = [];
+  
+  try {
+    const raw = await fs.readFile(
+      new URL("../sites.json", import.meta.url),
+      "utf8"
+    );
+  
+    if (raw.trim()) {
+      sitesArr = JSON.parse(raw);
+    } else {
+      console.warn("⚠️ File sites.json is empty порожній.");
+    }
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      console.warn("⚠️ File sites.json was not found.");
+    } else {
+      console.error("❌ Error while reading sites.json file:", err.message);
+    }
+  }
+
+  return sitesArr;
+}
+
+export const sitesUtils = {
   ping,
+  getSites,
 };
