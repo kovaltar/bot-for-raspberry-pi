1e5278be00d7c797829268d25571586b49b62a3a
kovaltar <kovaltar@gmail.com>
2025-08-15 17:52:19 +0200

    refactorimg:move statusStorage to sitesUtils

--- modules/sitesUtils.js
+++ modules/sitesUtils.js
@@ -71,8 +71,42 @@ function formatStatus({ site, status, ok }) {
   return `${site} | status: ${statusText} | ${ok ? "🟢 OK" : "🔴 Alert!"}`;
 }
 
+const sitesStatusFilePath = getFilePath(
+  "../data/sites-status.json",
+  import.meta.url
+);
+
+async function readStatuses() {
+  try {
+    const data = await fs.readFile(sitesStatusFilePath, "utf8");
+
+    return JSON.parse(data);
+  } catch {
+    console.error(
+      "❌ An error occurred while reading data/sites-status.json file:",
+      err.message
+    );
+  }
+}
+
+async function writeStatuses(results) {
+  const statusMap = {};
+
+  for (const { site, ok, status } of results) {
+    statusMap[site] = { ok, status };
+  }
+
+  await fs.writeFile(
+    sitesStatusFilePath,
+    JSON.stringify(statusMap, null, 2),
+    "utf8"
+  );
+}
+
 export const sitesUtils = {
   ping,
   getSites,
   formatStatus,
+  readStatuses,
+  writeStatuses,
 };
